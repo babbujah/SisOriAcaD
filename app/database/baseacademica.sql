313843a39d600c7d293d995e759740d9ce1e27d8
Renata Pitta <repitta@gmail.com>
2020-03-26 21:12:28 -0300

    Update baseacademica.sql

--- app/database/baseacademica.sql
+++ app/database/baseacademica.sql
@@ -1,99 +1,152 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE professor (
-  matricula VARCHAR(100)PRIMARY KEY NOT NULL,
+CREATE TABLE usuario (
+  matricula INTEGER PRIMARY KEY NOT NULL,
   nome VARCHAR(200),
   status VARCHAR(100) ,
   email VARCHAR(100),
-  dataIngresso DATE
+  dataIngresso DATE,
+  tipo char(1)
 );
-
-INSERT INTO professor VALUES('0001','Jair Fonseca', 'Ativo', 'jairfonseca@teste.com', '10/01/2000');
-INSERT INTO professor VALUES('0002','Paulo Guedes','Ativo' , 'pauloguedes@teste.com', '05/01/1980');
-INSERT INTO professor VALUES('0003','Jorge Lodi', 'Inativo', 'jorgelodi@teste.com','20/04/1995');
-INSERT INTO professor VALUES('0004','Samuel Lopes','Ativo' , 'samuellopes@teste.com','08/04/1985');
-
-CREATE TABLE aluno (
-  matricula VARCHAR(100)PRIMARY KEY NOT NULL,
-  nome VARCHAR(200),
-  status VARCHAR(100),
-  email VARCHAR(100),
-  data_ingresso DATE
-);
-
-INSERT INTO aluno VALUES('0005','Bruno César','Ativo', 'brunocesar@teste.com','02/02/2010');
-INSERT INTO aluno VALUES('0006','Willian Talles','Ativo', 'williantalles@teste.com','02/02/2010');
-INSERT INTO aluno VALUES('0007','Ágatha Gabrielly','Ativo', 'agathagabrielly@teste.com','02/02/2010');
-INSERT INTO aluno VALUES('0008','Adriel Cauã','Inativo' , 'adrielcaua@teste.com','02/02/2000');
-INSERT INTO aluno VALUES('0009','Andressa Mykahella','Ativo' , 'andressamykahella@teste.com','02/02/2000');
-INSERT INTO aluno VALUES(00010,'João Pedro','Ativo', 'joaopedro@teste.com','03/08/2015');
 
 CREATE TABLE componente_curricular (
-  cod_disciplina VARCHAR(100) PRIMARY KEY NOT NULL,
-  nome_disciplina VARCHAR(200),
-  nivel VARCHAR(50),
-  departamento VARCHAR(100),  
+  cod_componente INTEGER PRIMARY KEY NOT NULL,
+  departamento VARCHAR(50),
+  nome VARCHAR(100) ,
+  nivel VARCHAR(100),
   carga_horaria INTEGER,
-  tipo CHAR(1)
+  tipo char(1)
 );
 
-INSERT INTO componente_curricular VALUES('ECT1201','ALGEBRA LINEAR','Graduação','ECT','60','O');
-INSERT INTO componente_curricular VALUES('ECT1543','BANCO DE DADOS','Graduação','ECT','90','O');
-INSERT INTO componente_curricular VALUES('ECT1203','LINGUAGEM DE PROGRAMAÇÃO','Graduação','ECT','60','O');
-INSERT INTO componente_curricular VALUES('ECT1402','MECÂNICA DOS SÓLIDOS','Graduação','ECT','60','E');	
-	
 CREATE TABLE pre_requisito (
-  id INTEGER PRIMARY KEY NOT NULL,
-  componente_curricular_id VARCHAR(100) NOT NULL, 
-  pre_requisito_id VARCHAR(100) NOT NULL,
-  FOREIGN KEY(componente_curricular_id) REFERENCES componente_curricular(cod_disciplina), 
-  FOREIGN KEY(pre_requisito_id) REFERENCES componente_curricular(cod_disciplina)
+  cod_componente INTEGER,
+  pre_requisito INTEGER,
+  CONSTRAINT PK_pre_requisito PRIMARY KEY (cod_componente, pre_requisito),
+  FOREIGN KEY(cod_componente) REFERENCES componente_curricular(cod_componente), 
+  FOREIGN KEY(pre_requisito) REFERENCES componente_curricular(cod_componente)
 );
-/* tem que alterar essa forma de inserção, pq eu mudei os campos da tabela*/
-/*INSERT INTO pre_requisito VALUES(1,1,NULL);*/
-INSERT INTO pre_requisito VALUES(2,3,1);
-INSERT INTO pre_requisito VALUES(3,2,4);
-INSERT INTO pre_requisito VALUES(4,2,6);
-/*INSERT INTO pre_requisito VALUES(5,5,NULL);
-INSERT INTO pre_requisito VALUES(6,6,NULL);*/
-INSERT INTO pre_requisito VALUES(6,4,1);
-INSERT INTO pre_requisito VALUES(7,6,1);
-/*
+
 CREATE TABLE co_requisito (
-  id INTEGER PRIMARY KEY NOT NULL,
-  componente_curricular_id INTEGER NOT NULL, 
-  co_requisito_id INTEGER NOT NULL,
-  FOREIGN KEY(componente_curricular_id) REFERENCES componente_curricular(id), 
-  FOREIGN KEY(co_requisito_id) REFERENCES componente_curricular(id)
+  cod_componente INTEGER,
+  co_requisito INTEGER,
+  CONSTRAINT PK_co_requisito PRIMARY KEY (cod_componente, co_requisito),
+  FOREIGN KEY(cod_componente) REFERENCES componente_curricular(cod_componente), 
+  FOREIGN KEY(co_requisito) REFERENCES componente_curricular(cod_componente)
 );
-*/
-CREATE TABLE turma (
-  cod_turma INTEGER PRIMARY KEY NOT NULL,
-  horario VARCHAR(200),
-  ano_semestre VARCHAR(100) ,
-  departamento VARCHAR(100)
-);
+
 CREATE TABLE plano_curso (
-  cod_plano_curso INTEGER PRIMARY KEY NOT NULL,
-  data DATE
+  cod_plano INTEGER PRIMARY KEY NOT NULL,
+  matricula_aluno INTEGER, 
+  FOREIGN KEY(matricula_aluno) REFERENCES usuario(matricula)
 );
-CREATE TABLE notificacao (
-  cod_notificacao INTEGER PRIMARY KEY NOT NULL,
+
+CREATE TABLE plano_curso_componente (
+  cod_plano INTEGER,
+  cod_componente INTEGER,
+  CONSTRAINT PK_co_requisito PRIMARY KEY (cod_plano, cod_componente),
+  FOREIGN KEY(cod_componente) REFERENCES componente_curricular(cod_componente), 
+  FOREIGN KEY(cod_plano) REFERENCES plano_curso(cod_plano)
+);
+
+CREATE TABLE orientacao (
+  cod_orientacao INTEGER PRIMARY KEY NOT NULL,
+  cod_plano INTEGER,
+  matricula_professor INTEGER,
   data DATE,
-  assunto VARCHAR(200),
-  mensagem VARCHAR(500),
-  confirmacao CHAR(1),  
+  texto VARCHAR(200),
+  FOREIGN KEY(matricula_professor) REFERENCES usuario(matricula), 
+  FOREIGN KEY(cod_plano) REFERENCES plano_curso(cod_plano)
 );
-CREATE TABLE reuniao (
-  cod_reuniao INTEGER PRIMARY KEY NOT NULL,
-  data DATE,
-  horario VARCHAR(50),
-  assunto VARCHAR(200),
-  mensagem VARCHAR(500)  
-);
+
 CREATE TABLE curso (
   cod_curso INTEGER PRIMARY KEY NOT NULL,
-  nivel VARCHAR(50),
-  nome_curso VARCHAR(200)  
+  nome_curso VARCHAR(200),
+  nivel VARCHAR(50)
+);
+
+CREATE TABLE aluno (
+  matricula_aluno INTEGER,
+  cod_curso INTEGER,
+  CONSTRAINT PK_aluno PRIMARY KEY (matricula_aluno,cod_curso),
+  FOREIGN KEY(matricula_aluno) REFERENCES usuario(matricula), 
+  FOREIGN KEY(cod_curso) REFERENCES curso(cod_curso)
+);
+
+CREATE TABLE curso_componente (
+  cod_curso INTEGER,
+  cod_componente INTEGER,
+  CONSTRAINT PK_curso_componente PRIMARY KEY (cod_curso,cod_componente),
+  FOREIGN KEY(cod_curso) REFERENCES curso(cod_curso), 
+  FOREIGN KEY(cod_componente) REFERENCES componente_curricular(cod_componente)
+);
+
+CREATE TABLE historico_escolar (
+  matricula_aluno INTEGER,
+  cod_componente INTEGER,
+  nota FLOAT,
+  CONSTRAINT PK_historico_escolar PRIMARY KEY (matricula_aluno,cod_componente),
+  FOREIGN KEY(matricula_aluno) REFERENCES usuario(matricula), 
+  FOREIGN KEY(cod_componente) REFERENCES componente_curricular(cod_componente)
+);
+
+CREATE TABLE grupo (
+  cod_grupo INTEGER,
+  matricula_aluno INTEGER,
+  CONSTRAINT PK_grupo PRIMARY KEY (cod_grupo, matricula_aluno),
+  FOREIGN KEY(matricula_aluno) REFERENCES usuario(matricula)
+);
+
+CREATE TABLE reuniao (
+  cod_reuniao INTEGER PRIMARY KEY NOT NULL,
+  matricula_aluno INTEGER,
+  cod_grupo INTEGER,
+  assunto VARCHAR(200),
+  horario TIME,
+  mensagem VARCHAR(200),
+  data DATE,
+  FOREIGN KEY(matricula_aluno) REFERENCES usuario(matricula),
+  FOREIGN KEY(cod_grupo) REFERENCES grupo(PK_grupo)
+);
+
+CREATE TABLE notificao (
+  cod_notificacao INTEGER PRIMARY KEY NOT NULL,
+  assunto VARCHAR(200),
+  confirmacao CHAR,
+  mensagem VARCHAR(200),
+  data DATE
+);
+
+CREATE TABLE reuniao_notificacao (
+  cod_reuniao INTEGER,
+  cod_notificacao INTEGER,
+  CONSTRAINT PK_reuniao_notificacao PRIMARY KEY (cod_reuniao, cod_notificacao),
+  FOREIGN KEY(cod_reuniao) REFERENCES reuniao(cod_reuniao),
+  FOREIGN KEY(cod_notificacao) REFERENCES codificacao(cod_notificacao)
+);
+
+CREATE TABLE plano_notificacao (
+  cod_plano INTEGER,
+  cod_notificacao INTEGER,
+  CONSTRAINT PK_plano_notificacao PRIMARY KEY (cod_plano, cod_notificacao),
+  FOREIGN KEY(cod_plano) REFERENCES plano(cod_plano),
+  FOREIGN KEY(cod_notificacao) REFERENCES codificacao(cod_notificacao)
+);
+
+CREATE TABLE turma_aluno (
+  cod_turma INTEGER,
+  matricula_aluno INTEGER,
+  CONSTRAINT PK_turma_aluno PRIMARY KEY (cod_turma, matricula_aluno),
+  FOREIGN KEY(matricula_aluno) REFERENCES usuario(matricula)
+);
+
+CREATE TABLE turma (
+  cod_turma INTEGER PRIMARY KEY NOT NULL,
+  matricula_professor INTEGER,
+  cod_turma_aluno INTEGER,
+  ano_semestre VARCHAR(200),
+  horario TIME,
+  departamento VARCHAR(50),
+  FOREIGN KEY(matricula_professor) REFERENCES usuario(matricula),
+  FOREIGN KEY(cod_turma_aluno) REFERENCES turma_aluno(PK_turma_aluno)
 );
 
