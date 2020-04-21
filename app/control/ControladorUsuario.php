4375d7d309dad93840432c0cd5f797227295d889
Bruno César <bczion@hotmail.com>
2020-04-20 23:29:09 -0300

    Ajuste na função buscarUsuarioPorMatricula() e inserção da função buscarUsuario() para poder se adequadar ao encapsulamento da classe.

--- app/control/ControladorUsuario.php
+++ app/control/ControladorUsuario.php
@@ -1,5 +1,7 @@
 <?php
 abstract class ControladorUsuario{
+
+    const QNT_CARACTERES_MATRICULA = 1;
     
     // inserir usuario na base de dados
     public static function inserirUsuario( $usuario, $vinculo ){
@@ -8,26 +10,41 @@ abstract class ControladorUsuario{
     }
     
     // Busca usuario por nome
-    public function buscarUsuarioPorNome( $nomeUsuario ){
+    public function buscarUsuario( $dadoUsuario, $toArray = false ){
+        //verifica qual o dados do usuário que está sendo passado;
+        $tamanhoString = strlen( $dadoUsuario );
+        $primeiroCaracter = substr( $dadoUsuario, 0, 1 );
+         
+        if( $tamanhoString == self::QNT_CARACTERES_MATRICULA && is_numeric( $primeiroCaracter ) ){
+            $usuario = self::buscarUsuarioPorMatricula( $dadoUsuario, $toArray );
+            
+        }else{
+            $usuario = self::buscarUsuarioPorNome( $dadoUsuario, $toArray );
+            
+        }
+        
+        return $usuario;
+    }
+    
+    // Busca usuario por nome
+    public function buscarUsuarioPorNome( $nomeUsuario, $toArray = false ){
         //echo '<pre>'; print_r( $nomeAluno ); echo '</prep>';
         
         $usuarioDAO = new UsuarioDAO;
         
-        $usuario = $usuarioDAO->buscarUsuarioPorNome( $nomeUsuario );
+        $usuario = $usuarioDAO->buscarUsuarioPorNome( $nomeUsuario, $toArray );
         
         return $usuario;
     }
     
     // busca usuario por matrícula na base de dados
-    public function buscarUsuarioPorMatricula( $matriculaUsuario ){
+    public function buscarUsuarioPorMatricula( $matriculaUsuario, $toArray = false ){
     
-        $listaCaracterMatriculaTemp = str_split( $matriculaUsuario );
-        $primeiroCaracter = chr( $listaCaracterMatriculaTemp[0] );
-        echo $primeiroCaracter;
+        //echo '<pre>'; print_r( $nomeAluno ); echo '</prep>';
         
         $usuarioDAO = new UsuarioDAO;
         
-        $usuario = $usuarioDAO->buscarUsurioPorMatricula( $matriculaUsuario );
+        $usuario = $usuarioDAO->buscarUsuarioPorMatricula( $matriculaUsuario, $toArray );
         
         return $usuario;
     }  
