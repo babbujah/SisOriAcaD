a26fdaf7bcaa0feca81a8cf4add8834e18900d84
Bruno César <bczion@hotmail.com>
2020-04-27 14:56:51 -0300

    Inserção da classe AnoIngresso e adição do campo AnoIngresso na classe Aluno para controle e manipulação do ano e semestre de ingresso do usuário.

--- app/model/Aluno.php
+++ app/model/Aluno.php
@@ -3,13 +3,39 @@ class Aluno extends Usuario{
 
     public $historicoEscolarAluno;
     public $perfilAluno;
+    public $anoIngresso;
     
     public function __construct(){
         parent::__construct();
         $this->historicoEscolarAluno = [];
         $this->perfilAluno = [];
+        $this->anoIngresso = new AnoIngresso();
+         
     }
     
+    public function getAnoIngresso(){
+        return $this->anoIngresso;
+    }
+    
+    public function setAnoIngresso(){
+    
+        $tempData = explode( '/', $this->dataIngresso );
+        $tempMes = $tempData[1];
+        $tempAno = $tempData[2];        
+        
+        $this->anoIngresso->setAno( intval( $tempAno ) );
+        
+        if( intval( $tempMes ) < 1 || intval( $tempMes ) > 12 ){
+            throw new Exception('Mês fora do intervalo válido.');
+            
+        }elseif( intval( $tempMes ) <= 6 ){
+            $this->anoIngresso->setSemestre( 1 );
+            
+        }else{
+            $this->anoIngresso->setSemestre( 2 );
+        }
+    }
+    /*
     public function getHistoricoEscolarAluno(){
         return $this->historicoEscolarAluno;
     }
@@ -17,4 +43,5 @@ class Aluno extends Usuario{
     public function setHistoricoEscolarAluno( $historicoEscolarAluno ){
         $this->historicoEscolarAluno = $historicoEscolarAluno;
     }
+    */
 }
