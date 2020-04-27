573bacd4c4c85e7a15e32aac35c8a20d8af8d88a
Bruno César <bczion@hotmail.com>
2020-04-27 14:53:20 -0300

    Ajuste no retorno dos objetos de registro de usuário da classe UsuarioDAO.

--- app/control/UsuarioDAO.php
+++ app/control/UsuarioDAO.php
@@ -19,6 +19,33 @@ class UsuarioDAO extends TRecord{
         }
     }
     
+    // busca usuário por matrícula na base de dados
+    public function buscarUsuarioPorMatricula( $matriculaUsuario, $toArray = false ){
+                
+        try{
+            TTransaction::open( 'minhabasedados' );
+            //TTransaction::open( 'basedados' );
+            
+            if( $toArray ){
+                $repository = new RepositoryConstructor( 'Usuario' );
+                $usuario = $repository->where('matricula','=',$matriculaUsuario)->firstToArray(); 
+                              
+            }else {
+                $modeloUsuario = Usuario::where('matricula','=',$matriculaUsuario)->first();
+                $usuario = self::construirObjeto( $modeloUsuario );                
+                
+            }
+                
+            return $usuario;
+            
+            TTransaction::close();
+            
+        }catch( Exception $e ){
+            new TMessage( 'erro', $e->getMessage() );
+            
+        }
+    }
+    
     // busca usuário pelo nome de usuário na base de dados
     // obsoleta
     public function buscarUsuarioPorNome( $nomeUsuario, $toArray = false ){
@@ -47,27 +74,30 @@ class UsuarioDAO extends TRecord{
         }
     }
     
-    // busca usuário por matrícula na base de dados
-    public function buscarUsuarioPorMatricula( $matriculaUsuario, $toArray = false ){
-                
-        try{
-            TTransaction::open( 'minhabasedados' );
-            //TTransaction::open( 'basedados' );
+    private function construirObjeto( Usuario $usuario ){
+        if( $usuario->vinculo == 'A' ){
+            //echo 'Entrei no aluno';
+            $modeloUsuario = new Aluno();            
+                        
+        }elseif( $usuario->vinculo == 'P' ){
+            //echo 'Entrei no professor';
+            $modeloUsuario = new Professor();
             
-            if( $toArray ){
-                $repository = new RepositoryConstructor( 'Usuario' );
-                $usuario = $repository->where('matricula','=',$matriculaUsuario)->firstToArray();                
-            }else {
-                $usuario = Usuario::where('matricula','=',$matriculaUsuario)->first();
-            }
-                
-            return $usuario;
-            
-            TTransaction::close();
-            
-        }catch( Exception $e ){
-            new TMessage( 'erro', $e->getMessage() );
+        }else{
+            //echo 'Entrei no usuário';
+            $modeloUsuario = new Usuario();
             
         }
+        
+        $modeloUsuario->matricula = $usuario->matricula;
+        $modeloUsuario->nome = $usuario->nome;
+        $modeloUsuario->status = $usuario->status;                
+        $modeloUsuario->email =$usuario->email;
+        $modeloUsuario->dataIngresso = $usuario->dataIngresso;
+        $modeloUsuario->vinculo = $usuario->vinculo;
+               
+        //echo get_class( $modeloUsuario );
+        
+        return $modeloUsuario;
     }
 }
